8bdb47b7e5d15a9fe95ff0bf84661ad5b9430e4a
Drogaria Cidade <informatica@drogcidade.com.br>
2026-02-04 16:21:00 -0300

    Update download_table.py

--- scripts/download_table.py
+++ scripts/download_table.py
@@ -122,7 +122,7 @@ try:
             # COMPETENCIA AUTOMÁTICA
             campo = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="competencia"]')))
             campo.clear()
-            campo.send_keys("02/2026")
+            campo.send_keys("12/2025")
 
             # SELECT2
             select2 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#s2id_ordenar .select2-choice")))
@@ -172,7 +172,7 @@ try:
 
         campo = wait.until(EC.presence_of_element_located((By.XPATH, '//input[@id="competencia"]')))
         campo.clear()
-        campo.send_keys(competencia)
+        campo.send_keys(""12/2025)
 
         select2 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#s2id_ordenar .select2-choice")))
         select2.click()
